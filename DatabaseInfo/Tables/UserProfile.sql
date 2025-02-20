4bcb61311bd28734a6570432e45c155a0d1df835
Leonardo Carvalho <leonardotadeu.carvalho@gmail.com>
2025-02-20 02:20:15 -0300

    Add UserProfile table to store user information

---
+++ DatabaseInfo/Tables/UserProfile.sql
@@ -0,0 +1,20 @@
+/*
+  Tabela: UserProfile
+  Depende de: Nenhuma
+  Serve para: Armazena as informações do usuário
+*/
+CREATE TABLE UserProfile (
+    userId INTEGER PRIMARY KEY,        -- Identificador único do usuário
+    userType VARCHAR(20),              -- Tipo de usuário (ex: motorista, passageiro)
+    firstName VARCHAR(100) NOT NULL,   -- Primeiro nome do usuário
+    lastName VARCHAR(100),             -- Sobrenome do usuário
+    email VARCHAR(255),                -- Email do usuário
+    password VARCHAR(255),             -- Senha do usuário
+    phone VARCHAR(20),                 -- Telefone do usuário
+    birthdate DATE,                    -- Data de nascimento do usuário
+    createAt DATE,                     -- Data de criação do perfil
+    profilePicture VARCHAR(MAX),       -- URL ou caminho da foto de perfil do usuário
+    rating FLOAT,                      -- Avaliação do usuário
+    status VARCHAR(20),                -- Status do usuário (ex: ativo, inativo)
+    lastLogin DATE                     -- Data do último login do usuário
+);
