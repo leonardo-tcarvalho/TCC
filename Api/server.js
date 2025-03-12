878668529ef58f6a4b3a7c275a0bf2e27b81b76e
Leonardo Carvalho <leonardotadeu.carvalho@gmail.com>
2025-03-11 23:49:44 -0300

    Simplify CORS configuration and remove health check endpoint from server.js

--- Api/server.js
+++ Api/server.js
@@ -5,19 +5,13 @@ const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const corsOptions = {
-    origin: '*', // Permite todas as origens
-};
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
 connectDB();
 
 app.use('/api/users', userRoutes);
-app.get('/healt', (req, res) => {
-    res.send('API rodando!');
-});
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
