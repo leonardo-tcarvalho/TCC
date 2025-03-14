999ba33b7484d503ff267b5fdc939b5888abf173
Leonardo Carvalho <leonardotadeu.carvalho@gmail.com>
2025-03-14 01:04:17 -0300

    Remove default JWT secret to enforce environment variable usage

--- Api/middlewares/authMiddleware.js
+++ Api/middlewares/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET = process.env.JWT_SECRET || 'seuSegredoSuperSeguro';
+const JWT_SECRET = process.env.JWT_SECRET;
 
 function authMiddleware(req, res, next) {
     const token = req.header('Authorization');
