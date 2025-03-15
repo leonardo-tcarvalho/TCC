aa4dc73462d1dec182fd55a6b09178d4bf03094a
Leonardo Carvalho <leonardotadeu.carvalho@gmail.com>
2025-03-15 02:08:02 -0300

    Refactor AuthLoginScreen to use react-hook-form for form handling and validation

--- App/app/(auth)/index.tsx
+++ App/app/(auth)/index.tsx
@@ -1,58 +1,27 @@
-import InputBasic from "@/components/InputBasic";
-import ModalAlert from "@/components/ModalAlert";
 import api from "@/services/api";
-import { useState } from "react";
-import { KeyboardAvoidingView, ScrollView } from "react-native";
 import {
   StatusBar,
   StyleSheet,
+  SafeAreaView,
   Text,
   TouchableOpacity,
+  KeyboardAvoidingView,
+  ScrollView,
   View,
 } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useForm, Controller } from "react-hook-form";
+import { InputBasic, ModalAlert } from "@/components";
 
 export default function AuthLoginScreen() {
-  //
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const { control, handleSubmit, trigger } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onBlur",
+  });
 
-  // Modal
-  const [modalVisible, setModalVisible] = useState(false);
-  const [modalMessage, setModalMessage] = useState("");
-  const [typeMessage, setTypeMessage] = useState("");
-
-  const handleLogin = async () => {
-    let error = "";
-
-    try {
-      const response = await api.post("/users/login", {
-        email,
-        password,
-      });
-      if (response.status === 200) {
-        setModalMessage("Login successful!");
-        setTypeMessage("success");
-        setModalVisible(true);
-      }
-    } catch (error: any) {
-      console.error("Login error:", error);
-      setTypeMessage("error");
-      setModalMessage(error.response?.data?.message || "Error during login.");
-      setModalVisible(true);
-    }
-
-    if (modalVisible) {
-      return (
-        <ModalAlert
-          modalMessage={modalMessage}
-          visible={modalVisible}
-          onClose={() => setModalVisible(!modalVisible)}
-          typeMessage={typeMessage}
-        />
-      );
-    }
-  };
+  const onSubmit = (data: any) => console.log("Dados Finais:", data);
 
   return (
     <SafeAreaView style={styles.safeAreaView}>
@@ -68,23 +37,49 @@ export default function AuthLoginScreen() {
         >
           <View style={styles.containerLogin}>
             <Text style={styles.textTitle}>Welcome</Text>
-            <InputBasic
-              labelText="Email"
-              placeholderText="Enter your email..."
-              typeInput="email"
-              iconName="user"
-              onChangeText={setEmail}
-              value={email}
+            <Controller
+              control={control}
+              name="email"
+              render={({
+                field: { onChange, onBlur, value },
+                fieldState: { error },
+              }) => {
+                return (
+                  <InputBasic
+                    labelText="Email"
+                    placeholderText="Enter your email..."
+                    typeInput="email"
+                    iconName="envelope"
+                    value={value}
+                    onChangeText={(text) => onChange(text)}
+                    onBlur={onBlur}
+                    error={error?.message}
+                  />
+                );
+              }}
             />
-            <InputBasic
-              labelText="Password"
-              placeholderText="Enter your password..."
-              typeInput="password"
-              iconName="lock"
-              onChangeText={setPassword}
-              value={password}
+            <Controller
+              control={control}
+              name="password"
+              render={({
+                field: { onChange, onBlur, value },
+                fieldState: { error },
+              }) => {
+                return (
+                  <InputBasic
+                    labelText="Password"
+                    placeholderText="Enter your password..."
+                    typeInput="password"
+                    iconName="lock"
+                    value={value}
+                    onChangeText={(text) => onChange(text)}
+                    onBlur={onBlur}
+                    error={error?.message}
+                  />
+                );
+              }}
             />
-            <TouchableOpacity onPress={handleLogin} style={styles.button}>
+            <TouchableOpacity style={styles.button}>
               <Text>Sign in</Text>
             </TouchableOpacity>
           </View>
