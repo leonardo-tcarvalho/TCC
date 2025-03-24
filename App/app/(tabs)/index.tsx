189c7de66f556ed9fd5310a127da28225689729d
Leonardo Carvalho <leonardotadeu.carvalho@gmail.com>
2025-03-23 21:31:08 -0300

    Add user greeting in Home component and ensure AuthContext is available

--- App/app/(tabs)/index.tsx
+++ App/app/(tabs)/index.tsx
@@ -1,9 +1,19 @@
+import { AuthContext } from "@/contexts/AuthContext";
+import { useContext } from "react";
 import { Text, View } from "react-native";
 
 export default function Home() {
+  const authContext = useContext(AuthContext);
+
+  if (!authContext) {
+    throw new Error("AuthContext deve ser usado dentro de um AuthProvider");
+  }
+
+  const { user } = authContext;
+
   return (
     <View>
-      <Text>New</Text>
+      <Text>Olá {user?.name.split(" ")[0]}</Text>
     </View>
   );
 }
